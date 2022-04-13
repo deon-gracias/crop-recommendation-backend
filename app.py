5a2f8760e1c10edf850617cd6582c9d0b02005ef
Deon Gracias <deongracias1@gmail.com>
2022-04-13 17:41:10 +0530

    Added flask server

---
+++ app.py
@@ -0,0 +1,59 @@
+import sys
+import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import traceback
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+CORS(app)
+curr_loc = os.path.dirname(os.path.realpath(__file__))
+
+fertilizer = pd.read_csv(os.path.realpath(os.path.join(
+    curr_loc, "dataset/fertilizer.csv")))
+fertilizer.drop("Unnamed: 0", axis=1, inplace=True)
+
+model = joblib.load(os.path.join(curr_loc, "model.pkl"))
+print("Model loaded")
+
+
+@app.route("/")
+def index():
+    return "<h1>Crop Recommendation API</h1>"
+
+
+@app.route("/recommend", methods=["POST"])
+def recommend():
+    if model:
+        try:
+            reqData = request.json
+
+            prediction = model.predict(pd.DataFrame([reqData]))
+
+            return jsonify({"recommended": list(prediction)})
+        except:
+            return jsonify({"error": traceback.format_exc()})
+    else:
+        print("ML model not loaded.")
+
+
+@app.route("/crops", methods=["GET"])
+def getAllCrops():
+    try:
+        if request.args.get("name") is None:
+            return jsonify({"crops": list(fertilizer.T.to_dict().values())})
+
+        indexes = fertilizer.index[fertilizer["crop"]
+                                   == request.args.get("name")].tolist()
+
+        if len(indexes) < 1:
+            return jsonify({"error": "Crop Not Found"})
+
+        return jsonify({"crop": fertilizer.loc[indexes[0]].to_dict()})
+    except:
+        return jsonify({"error": traceback.format_exc()})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
